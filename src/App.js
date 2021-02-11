f36c78a24d1ccd848895574a47a8c5253f96ca0d
Kristian Pedersen <kp@Kristians-iMac.net>
2021-02-11 15:52:16 +0100

    Finpuss

--- src/App.js
+++ src/App.js
@@ -2,9 +2,9 @@
  * Table of contents:
  * 
  * 1. JavaScript
- *  1.1. Global state (list of users, toggle for all users' expanded view)
+ *  1.1. Global state (list of users)
  *  1.2. Get data from jsonplaceholder.com
- *  1.3. Toggle expanded view for individual users
+ *  1.3. Toggle expanded view for individual users (passed as prop to ClientInfo.js)
  *  1.4. Toggle expanded view for all users
  * 
  * 2. JSX
@@ -17,11 +17,10 @@ import './App.css'
 import { useState } from "react"
 import ClientInfo from "./components/ClientInfo"
 
+// 1. JavaScript
 export default function App() {
-  // 1. JavaScript
-  // 1.1. Global state (list of users, toggle for all users' expanded view)
+  // 1.1. Global state (list of users)
   const [users, setUsers] = useState([{}])
-  const [showAll, setShowAll] = useState(true)
 
   // 1.2. Get data from jsonplaceholder.com
   async function getData() {
@@ -31,25 +30,24 @@ export default function App() {
     setUsers(userInfo)
   }
 
-  // 1.3. Toggle expanded view for individual users
+  // 1.3. Toggle expanded view for individual users (passed as prop to ClientInfo.js)
   function toggleUser(name) {
-    const updated = users.map(function updateUserArray(user) {
+    const updatedUsers = users.map(user => {
       if (user.name === name) {
         return { ...user, expanded: !user.expanded }
       }
       return user
     })
-    setUsers(updated)
+    setUsers(updatedUsers)
   }
 
-  // 1.4. Toggle expanded view for all users
-  function expandAll(newState) {
-    setShowAll(newState)
+  // 1.4. Show or hide expanded view for all users
+  function showAllUserInfo(newState) {
     setUsers(users => {
-      const updatedUser = users.map(user => (
-        { ...user, expanded: newState }
-      ))
-      return updatedUser
+      const updatedUsers = users.map(user => {
+        return { ...user, expanded: newState }
+      })
+      return updatedUsers
     })
   }
 
@@ -57,24 +55,21 @@ export default function App() {
   return (
     <div className="App">
       {/* 2.1. Button: Fetch data */}
-      <button
-        onClick={async () => await getData()}
-        disabled={users.length > 1}
-      >Last inn data</button>
+      <button disabled={users.length > 1} onClick={async () => await getData()}>
+        Last inn data
+      </button>
 
       {users.length > 1 &&
         <>
-          <button onClick={() => expandAll(true)}>Vis all info</button>
-          <button onClick={() => expandAll(false)}>Skjul all info</button>
-          <div className="user-info">
-            {/* 2.2. Toggle all users */}
+          {/* 2.2. Toggle all users */}
+          <button onClick={() => showAllUserInfo(true)}>Vis all info</button>
+          <button onClick={() => showAllUserInfo(false)}>Skjul all info</button>
 
-            {/* 2.3. Display all users */}
-            {
-              users.map((user, index) => {
-                return <ClientInfo key={index} user={user} toggleUser={toggleUser} />
-              })
-            }
+          {/* 2.3. Display all users */}
+          <div className="user-info">
+            {users.map(function displayUser(user, index) {
+              return <ClientInfo key={index} user={user} toggleUser={toggleUser} />
+            })}
           </div>
         </>
       }
--- src/components/ClientInfo.js
+++ src/components/ClientInfo.js
@@ -1,20 +1,18 @@
-export default function ClientInfo(props) {
-	// Get user info and function for toggling individual user
-	const { user, toggleUser } = props
-	// Extract user info
-	const { name, email, expanded } = user
-	// Extract individual address parts
-	const { street, suite, city } = user.address
+export default function ClientInfo({ user, toggleUser }) {
+  // Extract user info
+  const { name, email, expanded } = user
+  // Extract individual address parts
+  const { street, suite, city } = user.address
 
-	return (
-		<>
-			<h1 onClick={() => toggleUser(name)}>{name}</h1>
-			{expanded &&
-				<>
-					<p>{`${street} ${suite}, ${city}`}</p>
-					<p><a href={`mailto:${email}`}>{email.toLowerCase()}</a></p>
-				</>
-			}
-		</>
-	)
+  return (
+    <>
+      <h1 onClick={() => toggleUser(name)}>{name}</h1>
+      {expanded &&
+        <>
+          <p>{`${street} ${suite}, ${city}`}</p>
+          <p><a href={`mailto:${email}`}>{email.toLowerCase()}</a></p>
+        </>
+      }
+    </>
+  )
 }
